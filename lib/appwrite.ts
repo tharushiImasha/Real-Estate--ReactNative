f0d523c1fdaa6e1ba09d426d29e9dd317bdc2de1
tharushiImasha <tharushi.byb@gmail.com>
2025-03-01 03:36:26 +0300

    Connect DB

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -15,6 +15,11 @@ export const config = {
     platform: "com.me.havenly",
     endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
     projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+    databaseId: process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID,
+    galleriesCollectionId: process.env.EXPO_PUBLIC_APPWRITE_GALLERIES_COLLECTION_ID,
+    reviewsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_REVIEWS_COLLECTION_ID,
+    agentsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_AGENTS_COLLECTION_ID,
+    propertiesCollectionId: process.env.EXPO_PUBLIC_APPWRITE_PROPERTIES_COLLECTION_ID,
 }
 
 export const client = new Client();
@@ -26,6 +31,7 @@ client
 
 export const avatar = new Avatars(client);
 export const account = new Account(client);
+export const databases = new Databases(client);
 
 export async function login() {
     try {
@@ -86,5 +92,59 @@ export async function getCurrentUser() {
     } catch (error) {
         console.log(error);
         return null;
+    }
+}
+
+export async function getLatestProperties() {
+    try {
+        const result = await databases.listDocuments(
+            config.databaseId!,
+            config.propertiesCollectionId!,
+            [Query.orderAsc("$createdAt"), Query.limit(5)]
+        );
+
+        return result.documents;
+    } catch (error) {
+        console.error(error);
+        return [];
+    }
+}
+
+export async function getProperties({
+                                        filter,
+                                        query,
+                                        limit,
+                                    }: {
+    filter: string;
+    query: string;
+    limit?: number;
+}) {
+    try {
+        const buildQuery = [Query.orderDesc("$createdAt")];
+
+        if (filter && filter !== "All")
+            buildQuery.push(Query.equal("type", filter));
+
+        if (query)
+            buildQuery.push(
+                Query.or([
+                    Query.search("name", query),
+                    Query.search("address", query),
+                    Query.search("type", query),
+                ])
+            );
+
+        if (limit) buildQuery.push(Query.limit(limit));
+
+        const result = await databases.listDocuments(
+            config.databaseId!,
+            config.propertiesCollectionId!,
+            buildQuery
+        );
+
+        return result.documents;
+    } catch (error) {
+        console.error(error);
+        return [];
     }
 }
